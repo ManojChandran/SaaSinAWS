9ca21953c1a4ea8a2e1f46a3924226bbfcfcc6da
ManojChandran <manoj.meparmbu@gmail.com>
2019-10-19 02:33:33 -0400

    added Cloud front distro

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "S3hosting" {
 
 # Deploy cloudformation distribution
 module "Cldfrntdistro" {
-  source       = "./modules/03_Cldfrntdistro"
+  source       = "./modules/03_Cldfrontdistro"
   domain_name  = "${var.domain_name}"
   hosted_website_bucket_name  = "${module.mys3hosting.tf_s3_hosted_bucket}"
 }
