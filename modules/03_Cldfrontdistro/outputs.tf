6901a0458de7aeafc802c9924a58c6e5abb76f14
ManojChandran <manoj.meparmbu@gmail.com>
2019-10-19 06:23:01 -0400

    cldfront testing

--- modules/03_Cldfrontdistro/outputs.tf
+++ modules/03_Cldfrontdistro/outputs.tf
@@ -1,5 +1,9 @@
 #----03_Cldfrontdistro/outputs.tf----
 
-output "tf_s3_dist_domain_name" {
-  value = "${aws_cloudfront_distribution.tf_s3_distribution.origin.domain_name}"
+output "tf_Cld_dist_domain_name" {
+  value = "${aws_cloudfront_distribution.tf_s3_distribution.domain_name}"
+}
+
+output "tf_Cld_dist_status" {
+  value = "${aws_cloudfront_distribution.tf_s3_distribution.status}"
 }
--- outputs.tf
+++ outputs.tf
@@ -4,5 +4,8 @@ output "S3 Hosted bucket name" {
   value = "${module.S3hosting.tf_s3_hosted_bucket}"
 }
 output "Cloudfront distrubution domain" {
-  value = "${module.Cldfrntdistro.tf_s3_dist_domain_name}"
+  value = "${module.Cldfrntdistro.tf_Cld_dist_domain_name}"
+}
+output "Cloudfront distrubution domain Status" {
+  value = "${module.Cldfrntdistro.tf_Cld_dist_status}"
 }
