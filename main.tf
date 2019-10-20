34af5ec7485c004d79251ac00983c8a5cdf0a54d
ManojChandran <manoj.meparmbu@gmail.com>
2019-10-20 05:21:28 -0400

    uncommented

--- main.tf
+++ main.tf
@@ -3,26 +3,26 @@
 provider "aws" {
   region = "${var.aws_region}"
 }
-#========commenting below for testing
-## Deploy s3 hosting bucket
-#module "S3hosting" {
-#  source       = "./modules/02_S3hosting"
-#  website_bucket_name  = "${var.website_bucket_name}"
-#}
-#
-## Deploy cloudformation distribution
-#module "Cldfrntdistro" {
-#  source       = "./modules/03_Cldfrontdistro"
-#  domain_name  = "${var.domain_name}"
-#  hosted_website_bucket_name  = "${module.S3hosting.tf_s3_hosted_bucket}"
-#}
-#
-## Deploy API gateway
-#module "Apigateway" {
-#  source       = "./modules/04_ApiGateway"
-#  api_rest_container_name = "${var.website_bucket_name}-rest-api"
-#}
-#========commenting above for testing
+
+# Deploy s3 hosting bucket
+module "S3hosting" {
+  source       = "./modules/02_S3hosting"
+  website_bucket_name  = "${var.website_bucket_name}"
+}
+
+# Deploy cloudformation distribution
+module "Cldfrntdistro" {
+  source       = "./modules/03_Cldfrontdistro"
+  domain_name  = "${var.domain_name}"
+  hosted_website_bucket_name  = "${module.S3hosting.tf_s3_hosted_bucket}"
+}
+
+# Deploy API gateway
+module "Apigateway" {
+  source       = "./modules/04_ApiGateway"
+  api_rest_container_name = "${var.website_bucket_name}-rest-api"
+}
+
 # Deploy VPC and attach IGW
 module "vpc_igw" {
   source       = "./modules/10_VPC_igw"
--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,21 @@
 #----root/outputs.tf-----
 
-#========commenting below for testing
-#output "S3 Hosted bucket name" {
-#  value = "${module.S3hosting.tf_s3_hosted_bucket}"
-#}
-#output "Cloudfront distrubution domain" {
-#  value = "${module.Cldfrntdistro.tf_Cld_dist_domain_name}"
-#}
-#output "Cloudfront distrubution domain Status" {
-#  value = "${module.Cldfrntdistro.tf_Cld_dist_status}"
-#}
-#output "API gateway ID" {
-#  value = "${module.Apigateway.tf_api_gateway_id}"
-#}
-#output "API gateway resource ID" {
-#  value = "${module.Apigateway.tf_api_root_resource_id}"
-#}
-#========commenting above for testing
+output "S3 Hosted bucket name" {
+  value = "${module.S3hosting.tf_s3_hosted_bucket}"
+}
+output "Cloudfront distrubution domain" {
+  value = "${module.Cldfrntdistro.tf_Cld_dist_domain_name}"
+}
+output "Cloudfront distrubution domain Status" {
+  value = "${module.Cldfrntdistro.tf_Cld_dist_status}"
+}
+output "API gateway ID" {
+  value = "${module.Apigateway.tf_api_gateway_id}"
+}
+output "API gateway resource ID" {
+  value = "${module.Apigateway.tf_api_root_resource_id}"
+}
+
 output "VPC ID" {
   value = "${module.vpc_igw.vpc_id}"
 }
