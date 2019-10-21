bf5fa442453fff6cd22c30848cae10c0fb7600f0
ManojChandran <manoj.meparmbu@gmail.com>
2019-10-20 23:03:24 -0400

    testing Flowlogs

--- modules/20_Flowlogs/main.tf
+++ modules/20_Flowlogs/main.tf
@@ -2,7 +2,7 @@
 
 resource "aws_flow_log" "tf_vpc_flowlogs" {
 #  iam_role_arn    = "${aws_iam_role.tf_IAM_flowlogs.arn}"
-#  log_destination = "${aws_cloudwatch_log_group.tf_cloudwatch.arn}"
+  log_destination = "${aws_cloudwatch_log_group.tf_cloudwatch.arn}"
   traffic_type    = "ALL"
   vpc_id          = "${var.vpc_id}"
 }
