112b8fa23b6073460e25e29253b88fe909b183f4
ManojChandran <manoj.meparmbu@gmail.com>
2019-10-25 14:28:04 -0400

    add terrfaorm 12 update

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,39 @@
 #----root/outputs.tf-----
 
-output "S3 Hosted bucket name" {
+output "S3-Hosted-bucket-name" {
   value = "${module.S3hosting.tf_s3_hosted_bucket}"
 }
-output "Cloudfront distrubution domain" {
+output "Cloudfront-distrubution-domain" {
   value = "${module.Cldfrntdistro.tf_Cld_dist_domain_name}"
 }
-output "Cloudfront distrubution domain Status" {
+output "Cloudfront-distrubution-domain-Status" {
   value = "${module.Cldfrntdistro.tf_Cld_dist_status}"
 }
-output "API gateway ID" {
+output "API-gateway-ID" {
   value = "${module.Apigateway.tf_api_gateway_id}"
 }
 
-output "VPC ID" {
+output "VPC-ID" {
   value = "${module.vpc_igw.vpc_id}"
 }
-output "VPC IGW" {
+output "VPC-IGW" {
   value = "${module.vpc_igw.igw_id}"
 }
-output "VPC Default Routetable" {
+output "VPC-Default-Routetable" {
   value = "${module.vpc_igw.default_route_table_id}"
 }
-output "VPC Cluster ID" {
+output "VPC-Cluster-ID" {
   value = "${module.ECSCluster.ecs_cluster_id}"
 }
-output "Public subnets" {
+output "Public-subnets" {
   value = ["${module.PublicSubnet.vpc_public_subnets}"]
 }
-output "Private subnets" {
+output "Private-subnets" {
   value = ["${module.PrivateSubnet.vpc_private_subnets}"]
 }
-output "RDS subnets" {
+output "RDS-subnets" {
   value = ["${module.RDSSubnet.vpc_RDS_subnets}"]
 }
-output "VPC Flowlogs" {
+output "VPC-Flowlogs" {
   value = "${module.Flowlogs.tf_VPC_flowlogs_id}"
 }
