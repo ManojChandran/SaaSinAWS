210e5c24b9c6dbb9694d7c9f429629d5b297adaf
ManojChandran <manoj.meparmbu@gmail.com>
2019-10-26 03:47:58 -0400

    added changes for terraform 12

--- modules/13_RDSSubnet/outputs.tf
+++ modules/13_RDSSubnet/outputs.tf
@@ -3,6 +3,6 @@
 output "vpc_RDS_subnets" {
   value = "${aws_subnet.tf_RDS_subnet.*.id}"
 }
-output "vpc_RDS_subnetsgroup" {
-  value = "${aws_db_subnet_group.tf_rds_subnetgroup.name}"
-}
+#output "vpc_RDS_subnetsgroup" {
+#  value = "${aws_db_subnet_group.tf_rds_subnetgroup.name}"
+#}
